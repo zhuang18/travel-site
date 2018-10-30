a646c3f623734add7d0133af6552cfad1eae4167
Zuya <jewelya25@gmail.com>
2018-10-30 10:49:05 -0400

    adding browser-sync, but having problem sync css file, code as known in gulpfile.js- task (cssInject)

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 gulp.task('default', async function(){
     console.log('gulp task is created!');
@@ -17,12 +18,30 @@ gulp.task('html',function(done){
 
 gulp.task('styles',function(){
     return gulp.src('./app/assets/styles/style.css')
-        .pipe(postcss([cssimport, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
         .pipe(gulp.dest('./app/temp/styles'));
     
 });
 
 gulp.task('watch', function() {
-    gulp.watch('./app/index.html',gulp.parallel('html'));
-    gulp.watch('./app/assets/styles/**/*.css',gulp.parallel('styles'));
-}); 
+
+    browserSync.init({
+        server: {
+            baseDir:"app"
+        }
+    })
+
+    gulp.watch('./app/index.html',function(done){
+        browserSync.reload();
+        done();
+    });
+
+
+    gulp.watch('./app/assets/styles/**/*.css',gulp.series('cssInject')); 
+})
+
+
+gulp.task('cssInject', gulp.series('styles'), function() {
+    return gulp.src('./app/temp/styles/style.css')
+        .pipe(browserSync.stream());
+});
